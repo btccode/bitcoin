4be6bdd35ed70a417bd55ae191c84f1c6cf024da
Mark Friedenbach <mark@friedenbach.org>
2015-02-24 14:03:38 -0800

    Rewrite CFeeRate to use CAmount internally, without dependency on CAMount’s internal structure. Requires modification of the serialization structure of CFeeRate.

--- src/amount.cpp
+++ src/amount.cpp
@@ -7,25 +7,19 @@
 
 #include "tinyformat.h"
 
-CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nSize)
-{
-    if (nSize > 0)
-        nSatoshisPerK = nFeePaid*1000/nSize;
-    else
-        nSatoshisPerK = 0;
-}
-
 CAmount CFeeRate::GetFee(size_t nSize) const
 {
-    CAmount nFee = nSatoshisPerK*nSize / 1000;
+    // calculate numerator
+    int64_t nFee = nFeePaid * nSize;
 
-    if (nFee == 0 && nSatoshisPerK > 0)
-        nFee = nSatoshisPerK;
+    // divide with integer arithmetic, rounding up
+    nFee = (nFee + (nPerBytes - 1)) / nPerBytes;
 
     return nFee;
 }
 
 std::string CFeeRate::ToString() const
 {
+    int64_t nSatoshisPerK = GetFeePerK();
     return strprintf("%d.%08d BTC/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN);
 }
--- src/amount.h
+++ src/amount.h
@@ -26,28 +26,40 @@ inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <=
 class CFeeRate
 {
 private:
-    CAmount nSatoshisPerK; // unit is satoshis-per-1,000-bytes
+    CAmount nFeePaid;  // amount of fee...
+    size_t  nPerBytes; // ...for this many bytes.
 public:
-    CFeeRate() : nSatoshisPerK(0) { }
-    explicit CFeeRate(const CAmount& _nSatoshisPerK): nSatoshisPerK(_nSatoshisPerK) { }
-    CFeeRate(const CAmount& nFeePaid, size_t nSize);
-    CFeeRate(const CFeeRate& other) { nSatoshisPerK = other.nSatoshisPerK; }
+    CFeeRate() : nFeePaid(0), nPerBytes(1) { }
+    explicit CFeeRate(const CAmount& _nFeePaid): nFeePaid(_nFeePaid), nPerBytes(1000) { }
+    CFeeRate(const CAmount& _nFeePaid, size_t _nPerBytes): nFeePaid(_nFeePaid), nPerBytes(_nPerBytes) { }
+    CFeeRate(const CFeeRate& other): nFeePaid(other.nFeePaid), nPerBytes(other.nPerBytes) { }
 
     CAmount GetFee(size_t size) const; // unit returned is satoshis
     CAmount GetFeePerK() const { return GetFee(1000); } // satoshis-per-1000-bytes
 
-    friend bool operator<(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK < b.nSatoshisPerK; }
-    friend bool operator>(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK > b.nSatoshisPerK; }
-    friend bool operator==(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK == b.nSatoshisPerK; }
-    friend bool operator<=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK <= b.nSatoshisPerK; }
-    friend bool operator>=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK >= b.nSatoshisPerK; }
+    friend bool operator<(const CFeeRate& a, const CFeeRate& b) { return a.nFeePaid * b.nPerBytes < b.nFeePaid * a.nPerBytes; }
+    friend bool operator>(const CFeeRate& a, const CFeeRate& b) { return a.nFeePaid * b.nPerBytes > b.nFeePaid * a.nPerBytes; }
+    friend bool operator==(const CFeeRate& a, const CFeeRate& b) { return a.nFeePaid * b.nPerBytes == b.nFeePaid * a.nPerBytes; }
+    friend bool operator<=(const CFeeRate& a, const CFeeRate& b) { return a.nFeePaid * b.nPerBytes <= b.nFeePaid * a.nPerBytes; }
+    friend bool operator>=(const CFeeRate& a, const CFeeRate& b) { return a.nFeePaid * b.nPerBytes >= b.nFeePaid * a.nPerBytes; }
     std::string ToString() const;
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(nSatoshisPerK);
+        // For compatability reasons the fee rate is serialized as a 64-bit
+        // integer. Note that this may involve some loss of precision and
+        // therefore is not round-trip-safe. However as currently used this
+        // does not pose a problem.
+        int64_t nFeePaidPerK;
+        if (!ser_action.ForRead())
+            nFeePaidPerK = GetFeePerK();
+        READWRITE(nFeePaidPerK);
+        if (ser_action.ForRead()) {
+            nFeePaid  = nFeePaidPerK;
+            nPerBytes = 1000;
+        }
     }
 };
 
