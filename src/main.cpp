7d404751bebac27bd96a4b4efacd5fc24fb795c2
Mark Friedenbach <mark@friedenbach.org>
2015-05-14 19:29:55 -0700

    Get rid of CTxIn::IsFinal(), which is only used in one location and whose semantics will not survive revival of sequence numbers, and introduce CTxIn::SEQUENCE_FINAL constant instead.

--- src/main.cpp
+++ src/main.cpp
@@ -642,7 +642,7 @@ bool IsFinalTx(const CTransaction &tx, int nBlockHeight, int64_t nBlockTime)
 {
     bool fFinalized = true;
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
-        fFinalized = fFinalized && txin.IsFinal();
+        fFinalized = fFinalized && (CTxIn::SEQUENCE_FINAL == txin.nSequence);
 
     if (!fFinalized && (int64_t)tx.nLockTime >= ((int64_t)tx.nLockTime < LOCKTIME_THRESHOLD ? (int64_t)nBlockHeight : nBlockTime))
         return false;
